1a997c22022342ffda4702ddc348b3f602819221
Tristan Mann <tb223one@gmail.com>
2024-01-12 15:35:45 -0500

    Made terminal clear

--- src/main.rs
+++ src/main.rs
@@ -43,6 +43,7 @@ fn main() {
 
 //used to display the main menu of the game
 fn main_menu(){
+    std::process::Command::new("clear").status().unwrap();
     println!("G.L.O.O.P: An adventure!\n 1.Play\n 2.Exit\n");
 }
 //used to gather and validate input from user
@@ -73,12 +74,15 @@ fn get_input(max:i32) -> i32{
 
 fn game(){
     let mainchar: Player = character_creation();
+    //This copmmad clears the console
+    std::process::Command::new("clear").status().unwrap();
     println!("Hello {} welcome to your adventure!", mainchar.name);
     combat(mainchar, random_enemy(initialize_enemies()));
 }
 
 fn character_creation() -> Player{
     let mut input: String = String::new();
+    std::process::Command::new("clear").status().unwrap();
     println!("What's your name"); 
     io::stdin().read_line(&mut input).expect("Not a real name bub");
     let mut mainchar = Player{
@@ -117,6 +121,7 @@ fn initialize_enemies() -> [Enemy; 3]{
 fn env_encounters(){
     println!("You walk through the tall grass when suddenly");
     //Add multiple encounters, which will occur at random and may or may not include combat
+    
 }
 
 fn random_enemy(enemy_array: [Enemy; 3]) -> Enemy{
@@ -132,42 +137,53 @@ fn random_enemy(enemy_array: [Enemy; 3]) -> Enemy{
 fn combat(mut mainchar: Player, mut opp: Enemy){
     let mut rng = rand::thread_rng();
     let mut fighting: bool = true;
-    while fighting{
-        println!("You find a wild {}\n 1.Attack\n 2.Heal\n", opp.enemy_type);
-        //Create container and get store user's input
-        let input = get_input(2);
-
-        if input == 1 {
-            opp.health -= rng.gen_range(0..mainchar.damage); 
-            println!("You hit the {} for {} damage it has {} health remaining", opp.enemy_type,"some", opp.health);
-        }
-        else if input == 2{
-            mainchar.health += 2;
-            println!("You healed for 2 health you have {} health left", mainchar.health);
-        }
-        if opp.health <= 0 || mainchar.health <= 0{
-            fighting = false;
-        }
+    println!("You find a wild {}", opp.enemy_type);
+    //Make player continue with the fight before beginning fight interaction
+    println!("1. Fight\n 2. Flee");
+    let mut input = get_input(2);
+    if input == 1{ 
+        std::process::Command::new("clear").status().unwrap();
+        while fighting{
+            println!("You decide to fight the {}\n 1.Attack\n 2. Heal", opp.enemy_type);
+            //Create container and get store user's input
+            input = get_input(2);
+            if input == 1 {
+                opp.health -= rng.gen_range(0..mainchar.damage); 
+                println!("You hit the {} for {} damage it has {} health remaining\n", opp.enemy_type,"some", opp.health);
+            }
+            else if input == 2{
+                mainchar.health += 2;
+                println!("You healed for 2 health you have {} health left\n", mainchar.health);
+            }
+            if opp.health <= 0 || mainchar.health <= 0{
+                fighting = false;
+            }
         //enemy turn next
-       if opp.health > 0{
-            //enemy's turn goes here now
-            mainchar.health -= rng.gen_range(0..opp.damage);
-            println!("You were hit for some damage by {} you have {} health left", opp.enemy_type, mainchar.health);
-       }
-       if opp.health <= 0 || mainchar.health <=0{
-            fighting = false;
-            if rng.gen_range(0..100) <20{
-                println!("You found an item");
+            if opp.health > 0{
+                //enemy's turn goes here now
+                mainchar.health -= rng.gen_range(0..opp.damage);
+                println!("You were hit for some damage by {} you have {} health left\n", opp.enemy_type, mainchar.health);
             }
-            else{
-                println!("You find nothing");
+        if opp.health <= 0 || mainchar.health <=0{
+                fighting = false;
+                if rng.gen_range(0..100) <20{
+                    println!("You found an item");
+                }
+                else{
+                    println!("You find nothing");
+                }
             }
-       }
+        }
+    }
+    else{
+        println!("You run from the {}", opp.enemy_type);
+        fighting = false;
     }
 }
 
 fn exit() -> bool{
     let input: i32;
+    std::process::Command::new("clear").status().unwrap();
     println!("Exit?\n 1.Yes\n 2.No");
     input = get_input(2);
     if input == 1{
